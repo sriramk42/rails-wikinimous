19c37e6030c997806c15026842f2212704ec4aac
Sriram Krishnan <sriramk1986@gmail.com>
2019-11-05 17:28:52 +0900

    Working code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Creating 10 articles..."
+
+10.times do
+  article = Article.new(
+    title: Faker::DcComics.name,
+    content: Faker::DcComics.title
+  )
+  article.save!
+end
+
+puts "Finished creating 10 articles..."
